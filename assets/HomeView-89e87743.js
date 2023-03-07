2222594e37cb6c92ee13b4366790346382ba07b5
yun <ziyunn99@gmail.com>
2023-03-07 15:20:46 +0800

    deploy

---
+++ assets/HomeView-89e87743.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s}from"./index-eaab571c.js";const c={components:{}},n={class:"header",style:{"background-image":"url(./src/images/home/banner1@3x.png)","background-size":"cover",height:"100vh"}};function r(t,a,i,_,p,d){return o(),s("div",n)}const h=e(c,[["render",r]]);export{h as default};
