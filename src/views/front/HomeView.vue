6a72880715a0e69f5ae9ad9e911471a38f765e9c
yun <ziyunn99@gmail.com>
2023-03-09 23:47:37 +0800

    feat: 新增熱門活動

--- src/views/front/HomeView.vue
+++ src/views/front/HomeView.vue
@@ -6,30 +6,50 @@
       <!-- <div>搜尋</div> -->
     </div>
   </div>
+  <div class="hotlist text-light">
+    <div class="container content"  style="padding-top: 80px;">
+      <h2 style="margin-bottom: 40px;">熱門活動</h2>
+      <div class="row row-cols-4">
+        <div class="col hot-1 d-flex align-items-start flex-column p-0"
+              style="height: 320px; position: absolute;">
+          <div class="d-flex justify-content-between" style="width: 100%; margin-top: 20px;">
+            <h5 class="d-block"
+              style="padding: 8px 16px; background: rgba(10, 96, 60, 0.6);">
+            即將額滿</h5>
+            <a href="#" class="d-block favorite"
+              style="height: 36.33px; width: 33.33px; margin-right: 22px;">
+            </a>
+          </div>
+          <!-- <h4 class="position-relative " style="padding: 20px 8px; top: 230px">搭車比登山累—郡大山</h4> -->
+          <h4 class="mt-auto mb-0" style="padding: 20px 20px">搭車比登山累—郡大山</h4>
+        </div>
+      </div>
+    </div>
+  </div>
     <!-- 中間輪播 -->
     <!-- Swiper -->
-    <div>
-        <swiper :modules="modules" class="swiper"
-        :pagination="{ dynamicBullets: true, clickable: true }" style="position: relative; height: 480px;">
-          <swiper-slide class="swiper-slide swiper-slide-1">
-              <div class="d-flex flex-column justify-content-center align-items-center text-white" style="height: 100%; border: 1px solid #FFFFFF;">
-                  <h3>自然之美</h3>
-                  <h3>。</h3>
-                  <h3>森呼吸</h3>
-              </div>
-          </swiper-slide>
-          <swiper-slide class="swiper-slide swiper-slide-2" style="background-position: 50% 50%;">
-              <div class="d-flex flex-column justify-content-center align-items-center text-white" style="height: 100%; border: 1px solid #FFFFFF;">
-                  <h3>寧靜 美好</h3>
-              </div>
-          </swiper-slide>
-          <swiper-slide class="swiper-slide swiper-slide-3" style="background-position: 50% 30%;">
-              <div class="d-flex flex-column justify-content-center align-items-center text-white" style="height: 100%; border: 1px solid #FFFFFF;">
-                  <h3>自我對話</h3>
-              </div>
-          </swiper-slide>
-        </swiper>
-    </div>
+  <div>
+    <swiper :modules="modules" class="swiper"
+    :pagination="{ dynamicBullets: true, clickable: true }" style="position: relative; height: 480px;">
+      <swiper-slide class="swiper-slide swiper-slide-1">
+          <div class="d-flex flex-column justify-content-center align-items-center text-white" style="height: 100%; border: 1px solid #FFFFFF;">
+              <h3>自然之美</h3>
+              <h3>。</h3>
+              <h3>森呼吸</h3>
+          </div>
+      </swiper-slide>
+      <swiper-slide class="swiper-slide swiper-slide-2" style="background-position: 50% 50%;">
+          <div class="d-flex flex-column justify-content-center align-items-center text-white" style="height: 100%; border: 1px solid #FFFFFF;">
+              <h3>寧靜 美好</h3>
+          </div>
+      </swiper-slide>
+      <swiper-slide class="swiper-slide swiper-slide-3" style="background-position: 50% 30%;">
+          <div class="d-flex flex-column justify-content-center align-items-center text-white" style="height: 100%; border: 1px solid #FFFFFF;">
+              <h3>自我對話</h3>
+          </div>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -74,5 +94,39 @@ export default {
 }
 .swiper-slide-3{
   background-image: url(@/images/home/與自我對話.jpg)
+}
+.hotlist{
+  position: relative;
+  background-image: url(@/images/home/BG-HOT@3x.png);
+  background-size: cover;
+  height: 559px;
+}
+.hotlist::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+}
+.hotlist .content{
+  position: relative;
+}
+.hot{
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4));
+  box-shadow: 8px 8px 20px rgba(0, 0, 0, 0.16);
+}
+.hot-1{
+  background-image: url(@/images/home/熱門活動01@3x.png);
+  background-size: cover;
+}
+.favorite{
+  background-image: url(@/images/icon/favorite.svg);
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+.favorite:hover{
+  background-image: url(@/images/icon/favorite-hover.svg);
 }
 </style>
