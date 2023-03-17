510f0573af3a5ec95cdde81860835f93822d3189
yun <ziyunn99@gmail.com>
2023-03-17 14:15:21 +0800

    feat: 更改名稱

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
     })
   ],
-  base: '/vue-hw-7/',
+  base: '/the-mountains/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
