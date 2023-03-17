0ffd19549a7eb50b9dd409e6f54dd754b2527412
yun <ziyunn99@gmail.com>
2023-03-17 14:16:58 +0800

    deploy

---
+++ assets/loadingStore-cc88de8f.js
@@ -0,0 +1,1 @@
+import{i}from"./index-47d87b33.js";const a=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loadingTrue(){this.isLoading=!0},loadingFalse(){this.isLoading=!1}}});export{a as l};
