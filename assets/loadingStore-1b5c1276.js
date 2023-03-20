db08f133bea29836aa3ae713f318a8cbcec134b3
yun <ziyunn99@gmail.com>
2023-03-21 00:34:29 +0800

    deploy

--- assets/loadingStore-1b5c1276.js
+++
@@ -1,1 +0,0 @@
-import{i}from"./index-8b3942b4.js";const a=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loadingTrue(){this.isLoading=!0},loadingFalse(){this.isLoading=!1}}});export{a as l};
---
+++ assets/loadingStore-abd78020.js
@@ -0,0 +1,1 @@
+import{d as i}from"./pinia-e6b04fcb.js";const a=i("loadingStore",{state:()=>({isLoading:!1}),actions:{loadingTrue(){this.isLoading=!0},loadingFalse(){this.isLoading=!1}}});export{a as l};
